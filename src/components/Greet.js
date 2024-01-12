ec2193ee49697a1571f7cb1f19054e5d6c41bf2e
zhusophia <iceyes123@gmail.com>
2024-01-11 22:10:00 -0500

    test

---
+++ src/components/Greet.js
@@ -0,0 +1,27 @@
+function Greet() {
+    return <h1>Hello Ice {2 + 2}</h1> // this is actually jsx and not html
+}
+
+// same thinhg as 
+// const Greet = () => { 
+//    return <h1>hi</h1>
+//}
+// the () probably means function, and it's jsut saved to a variable 
+// instead of explictily writing 'function'
+
+/*
+
+function Pen(name, color, price) {
+    this.name = name;
+    this.color = color;
+    this.price = price;
+}
+
+const pen1 = new Pen("Marker", "Blue", "$3");
+console.log(pen1);
+
+28:04
+
+*/
+
+export default Greet
