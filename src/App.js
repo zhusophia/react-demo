c107fafea351c2b1a62210da69b279a36dbb9c20
zhusophia <iceyes123@gmail.com>
2024-01-14 23:56:27 -0500

    33:06

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import './App.css';
 // use the component within the html
 
 // the first 'Greet' doesn't actually matter, it's just a variable name
+// <greet></greet> = <greet />
+
+// props = optional input that commands accept (so think of it like an variable that takes input)
+// makes things dynamic 
+
 import Greet from './components/Greet'
 
 function App() {
   return (
     <div className="App">
-     <Greet></Greet>
+     <Greet name ='Bruce'/>
+     <Greet name = "Wildons"/>
+     <Greet name = "test"/>
     </div>
   );
 }
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,4 +1,10 @@
-function Greet() {
+export const Greet = (props) => {
+    return <h1>Hello {props.name}</h1>
+}
+
+
+
+/*function Greet() {
     return <h1>Hello Ice {2 + 2}</h1> // this is actually jsx and not html
 }
 
@@ -9,7 +15,9 @@ function Greet() {
 // the () probably means function, and it's jsut saved to a variable 
 // instead of explictily writing 'function'
 
-/*
+// classes are basically equivalent to functions? allows for transferability
+
+
 
 function Pen(name, color, price) {
     this.name = name;
@@ -22,6 +30,9 @@ console.log(pen1);
 
 28:04
 
+
+
 */
 
 export default Greet
+
